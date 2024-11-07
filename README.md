1019f5b057ef5c5006f5cc1b284293a2fce46055
devin <devin.lighgheart@gmail.com>
2024-11-07 08:52:34 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Unit three career simulatoin: book buddy
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+ 
+console.log(`hello console`)
+  return (
+    <>
+     <h2>Hello</h2>
+    </>
+  )
+}
+
+export default App
